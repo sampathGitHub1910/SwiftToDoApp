6e9f9888963bad160af91f668b405b2f8ea7900d
Sai Sampath Pichika <saisampath1910@gmail.com>
2024-06-10 18:13:18 +0530

    ToDo App

---
+++ ToDoApp/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ToDoApp
+//
+//  Created by RPS on 10/06/24.
+//
+
+import Foundation
---
+++ ToDoApp/Views/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  ToDoApp
+//
+//  Created by RPS on 10/06/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
